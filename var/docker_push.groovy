1f0cb0e1a8e5c457e802564de3feafdea1b1df19
Deepak Patel <deepakpatel1029@gmail.com>
2025-04-06 16:49:37 +0530

    Create docker_push.groovy

---
+++ var/docker_push.groovy
@@ -0,0 +1,13 @@
+def call(String credId , String ImageName){
+   stage("Push to Docker Hub") {
+            steps {
+                withCredentials([usernamePassword(
+                    credentialsId: "${credId}",
+                    usernameVariable: "DockerHubUser",
+                    passwordVariable: "DockerHubPass"
+                    )]){
+                        sh "docker login -u ${env.DockerHubUser} -p ${env.DockerHubPass}"
+                        sh "docker image tag ${ImageName} ${env.DockerHubUser}/${ImageName}
+                        sh "docker push ${env.DockerHubUser}/flask-app:latest"
+                    }
+            }
